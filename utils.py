fcf8df2aa7470a1aaba91f4e3fe6dbfd5e84ffe8
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-10-05 23:58:47 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -670,8 +670,6 @@ async def send_all(bot, userid, files, ident):
                         [
                         InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                         InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                    ],[
-                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")
                         ]
                     ]
                 )
